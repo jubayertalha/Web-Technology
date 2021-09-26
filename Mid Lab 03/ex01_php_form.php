30732a2ee87f5f80222234a2cbfc218403a90c74
Talha Jubayer <jubayertalha194@gmail.com>
2021-09-26 09:14:49 +0600

    add mid lab 03 ex 01 form welcome

---
+++ Mid Lab 03/ex01_php_form.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <form action="ex01_php_welcome.php" method="POST">
+            <br>Name: <input type="text" name="name"><br>
+            <br>Email: <input type="email" name="email"><br>
+            <br>Phone: <input type="phone" name="phone"><br>
+            <br>Submit <input type="submit" name="submit"><br>
+        </form>
+    </body>
+</html>
---
+++ Mid Lab 03/ex01_php_welcome.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        <br>Welcome <?php echo $_POST["name"]; ?><br>
+        <br>Your email address is: <?php echo $_POST["email"]; ?><br>
+        <br>Your phone number is: <?php echo $_POST["phone"]; ?><br>
+    </body>
+</html>
