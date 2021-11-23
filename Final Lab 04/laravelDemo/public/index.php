5831f12d6645274c5eb88bd39edbafecd709f513
Talha Jubayer <jubayertalha194@gmail.com>
2021-11-23 12:03:08 +0600

    add final lab 04 demo laravel project

---
+++ Final Lab 04/laravelDemo/public/index.php
@@ -0,0 +1,25 @@
+<?php ?>
+<html>
+    <head>
+        <style>
+            table, th, td{
+                border: 1px solid black;
+            }
+            table{
+                width: 400px;
+            }
+        </style>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <td>Name</td>
+                <td>Rahman, Sheikh Talha Jubayer</td>
+            </tr>
+            <tr>
+                <td>ID</td>
+                <td>19-41468-3</td>
+            </tr>
+        </table>
+    </body>
+</html>
